6b1ebf142701076221ea6fac80d59bfcb220d540
Abdal Hussain <abdalhussain5790@gmail.com>
2025-11-25 18:35:32 +0500

    Fetch data from Backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,15 @@ import { Header } from '../components/Header'
 import './HomePage.css'
 import {products} from '../../starting-code/data/products.js'
 
-console.log(products)
 export function HomePage(){
+
+    fetch('http://localhost:3000/api/products')
+            .then((response)=>{
+            return  response.json();
+            }).then((data)=>{
+                console.log(data)
+            })
+        
     return(
     <>
     <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
