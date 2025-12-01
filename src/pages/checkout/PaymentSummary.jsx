13606a02a2d3829b7c3f53b108c1844a9906d38d
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-01 20:48:48 +0500

    Make place order work

--- src/pages/checkout/PaymentSummary.jsx
+++ src/pages/checkout/PaymentSummary.jsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import { formatCurrency } from "../../utils/money";
+import { useNavigate } from "react-router";
 
-
-export function PaymentSummary({paymentSummary}) {
+export function PaymentSummary({paymentSummary, loadCart}) {
+  const navigate = useNavigate();
+  const createOrder = async ()=>{
+    await axios.post('/api/orders');
+    await loadCart();
+    navigate('/orders')
+  }
   return (
     <div className="payment-summary">
       <div className="payment-summary-title">Payment Summary</div>
@@ -42,7 +49,7 @@ export function PaymentSummary({paymentSummary}) {
             </div>
           </div>
 
-          <button className="place-order-button button-primary">
+          <button className="place-order-button button-primary" onClick={createOrder}>
             Place your order
           </button>
         </>
