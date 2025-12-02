1fd73971cd350a3739e6cc56bbb0b52b01706e1c
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-02 21:30:12 +0500

    create feature of showing added message on click add to cart button

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -4,6 +4,7 @@ import { formatCurrency } from "../../utils/money";
 
 export function Product({product,loadCart}) {
     const [quantity, setQuantity] = useState(1);
+    const [showAddedMessage, setShowAddedMessage] = useState(false);
 
     const addToCart = async () => {
         await axios.post("/api/cart-items", {
@@ -12,7 +13,13 @@ export function Product({product,loadCart}) {
         });
 
         await loadCart();
+        setShowAddedMessage(true)
+        
+        setTimeout(()=>{
+          setShowAddedMessage(false)
+        },2000)
     }   
+
 
     const selectQuantity = (event) => {
         const selectedQuantity = Number(event.target.value);
@@ -59,7 +66,7 @@ export function Product({product,loadCart}) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{opacity : showAddedMessage ? 1 : 0}}>
         <img src="images/icons/checkmark.png" />
         Added
       </div>
