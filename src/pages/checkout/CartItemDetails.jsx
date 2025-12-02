b59462743be4f2f27936bd00731fe33a175d707c
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-02 22:02:45 +0500

    create update button interactive

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,13 +1,31 @@
 import axios from "axios";
 import { formatCurrency } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({cartItem, loadCart}) {
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false)
+
+  const [quantity, setQuantity] = useState(cartItem.quantity)
 
   const deleteCartItem = async ()=>{
     await axios.delete(`/api/cart-items/${cartItem.productId}`)
     await loadCart();
   }
   
+  const isUpdateQuantity = async ()=> {
+
+    await axios.put(`/api/cart-items/${cartItem.productId}`,{
+      quantity: Number(quantity)
+    })
+
+    await loadCart();
+
+    setIsUpdatingQuantity(prev => !prev) // toggle the inputbox with quantity
+  }
+
+  const updateCartItem = (event)=>{
+    setQuantity(event.target.value)
+  }
   return (
     <>
       <img className="product-image" src={cartItem.product.image} />
@@ -19,9 +37,11 @@ export function CartItemDetails({cartItem, loadCart}) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:{ isUpdatingQuantity 
+            ? <input type="text" className="update-input-box" value={quantity} onChange={updateCartItem}/>
+            : <span className="quantity-label">{cartItem.quantity}</span>}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span className="update-quantity-link link-primary" onClick={isUpdateQuantity}>Update</span>
           <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>Delete</span>
         </div>
       </div>
