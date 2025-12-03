da3fa3be82a397bdb0fa8dfe2dcd314b79ddcb39
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-03 21:54:28 +0500

    use beforeEach to remove duplication

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import {expect, it, describe,vi} from 'vitest'
+import {expect, it, describe,vi, beforeEach} from 'vitest'
 import { render, screen } from '@testing-library/react';
 import {Product} from './Product'; 
 import userEvent from '@testing-library/user-event'; // for button testing
@@ -7,8 +7,12 @@ import axios from 'axios';
 vi.mock('axios')
 
 describe('Product component',()=>{
-    it('displays the product details correctly',()=>{
-        const product = {
+    let product;
+
+    let loadCart;
+    
+    beforeEach(()=>{
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,7 +24,10 @@ describe('Product component',()=>{
             keywords: ["socks", "sports", "apparel"]
         };
 
-        const loadCart = vi.fn(); // vi.fn() create fake version of loadCart that doesn't do anything
+        loadCart = vi.fn();// vi.fn() create fake version of loadCart that doesn't do anything
+    })
+
+    it('displays the product details correctly',()=>{
         //for integration test or component testing we will use render()
         render(<Product product={product} loadCart={loadCart}/>)
 
@@ -37,20 +44,6 @@ describe('Product component',()=>{
     });
 
     it('adds a product to the cart', async()=>{
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-            stars: 4.5,
-            count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-
-        const loadCart = vi.fn();
-
         render(<Product product={product} loadCart={loadCart}/>)
 
         const user = userEvent.setup();
