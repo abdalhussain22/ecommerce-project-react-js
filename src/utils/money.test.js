8cfa643a27870c0d95c7f7893d9b2262c44a44d6
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-03 16:51:46 +0500

    Create unit tests for formatMoney

---
+++ src/utils/money.test.js
@@ -0,0 +1,13 @@
+import {expect, it, describe} from 'vitest'
+import {formatCurrency} from './money'; 
+
+describe('formatMoney',()=>{
+    it('formats 1999 cents as $19.99',()=>{
+        expect(formatCurrency(1999)).toBe('$19.99');
+    });
+    
+    it('displays two decimals',()=>{
+        expect(formatCurrency(1090)).toBe('$10.90');
+        expect(formatCurrency(100)).toBe('$1.00');
+    })
+})
