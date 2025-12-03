77434e740a71832ad030a34ef752c2b63ca20bc6
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-03 21:26:46 +0500

    Create Integration test for product component

---
+++ src/pages/home/Product.test.jsx
@@ -0,0 +1,34 @@
+import {expect, it, describe,vi} from 'vitest'
+import { render, screen } from '@testing-library/react';
+import {Product} from './Product'; 
+
+describe('Product component',()=>{
+    it('displays the product details correctly',()=>{
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+            stars: 4.5,
+            count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+
+        const loadCart = vi.fn(); // vi.fn() create fake version of loadCart that doesn't do anything
+        //for integration test or component testing we will use render()
+        render(<Product product={product} loadCart={loadCart}/>)
+
+        // screen is used to render fake web page
+        expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument(); // for text test
+
+        expect(screen.getByText('$10.90')).toBeInTheDocument();// text
+
+        expect(screen.getByTestId('product-image')).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg') //for image test
+
+        expect(screen.getByTestId('rating-image')).toHaveAttribute('src',`images/ratings/rating-45.png`) //image
+ 
+        expect(screen.getByText('87')).toBeInTheDocument(); //text
+    });
+})
