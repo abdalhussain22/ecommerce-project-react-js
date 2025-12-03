b8b27e3a6ebfa2cc9ea3b019b05ce3a597bb7396
Abdal Hussain <abdalhussain5790@gmail.com>
2025-12-03 22:36:39 +0500

    Create integration test for Home page

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -27,7 +27,7 @@ export function Product({product,loadCart}) {
             console.log(selectedQuantity);
         }
   return (
-    <div key={product.id} className="product-container">
+    <div key={product.id} className="product-container" data-testid="product-container">
       <div className="product-image-container">
         <img className="product-image" data-testid='product-image' src={product.image} />
       </div>
